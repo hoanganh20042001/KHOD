1d809286b106b33df679bfb3661c6f2dc7bbbff9
hoanganh20042001 <103996474+hoanganh20042001@users.noreply.github.com>
2022-12-29 18:24:16 +0700

    s

--- KHOD/GUI/frmNhapKho.cs
+++ KHOD/GUI/frmNhapKho.cs
@@ -27,6 +27,8 @@ namespace KHOD.GUI
 
 			NhapF nhap = new NhapF();
 			dataGridView1.DataSource = nhap.NhapNgay(date1.Value.Date);
+			thanhtoan.Items.Add("Trực tiếp");
+			thanhtoan.Items.Add("Thẻ ngân hàng");
 		}
 
 		private void simpleButton1_Click(object sender, EventArgs e)
@@ -34,10 +36,57 @@ namespace KHOD.GUI
 			NHAP_KHO nk = new NHAP_KHO();// khoi taoj nhap kho
 			HOA_DON hd = new HOA_DON();//khoir tao hoa don
 			NhapF nhap = new NhapF();// thong tin cac san pham nhap
+			int madon = nhap.NhapNgay(date1.Value.Date)[1].MaDon;
+			hd.MaDon = madon;
+			hd.HinhThucThanhToan = thanhtoan.Text;
+
+			hd.Thanhtien = db.Database.SqlQuery<double>("select sum(dongia*soluong) from DAT_NL where madon=@madon", new SqlParameter("@madon",madon)).SingleOrDefault();
+			hd.NgayLap = DateTime.Now.Date;
+			db.HOA_DON.Add(hd);
+			db.SaveChanges();
+			nk.MaHD = hd.MaHD;
+			nk.MaNV = 4;
+			nk.NgayLap = date1.Value.Date;
+			db.NHAP_KHO.Add(nk);
+			db.SaveChanges();
 			foreach (var item in nhap.NhapNgay(date1.Value.Date))
 			{
-
+				NGUYEN_LIEU nl = new NGUYEN_LIEU();
+				nl.NgaySanXuat = date1.Value.Date;
+				if (item.MaLoai == 1)
+				{
+					nl.HanSuDung = date1.Value.Date.AddDays(30);
+				}
+				else
+					nl.HanSuDung = date1.Value.Date.AddDays(1);
+				nl.MaTP = item.MaTP;
+				nl.MaNK = nk.MaNK;
+				nl.SoLuong = item.SoLuong;
+				nl.TrangThai = true;
+				db.NGUYEN_LIEU.Add(nl);
 			}
+			db.SaveChanges();
+		}
+
+		private void panel1_Paint(object sender, PaintEventArgs e)
+		{
+
+		}
+
+		private void lbten_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void txtSl_TextChanged(object sender, EventArgs e)
+		{
+
+		}
+
+		private void date1_ValueChanged(object sender, EventArgs e)
+		{
+			NhapF nhap = new NhapF();// thong tin cac san pham nhap
+			dataGridView1.DataSource = nhap.NhapNgay(date1.Value.Date);
 		}
 	}
 	
--- KHOD/GUI/frmXuatKho.Designer.cs
+++ KHOD/GUI/frmXuatKho.Designer.cs
@@ -1,7 +1,7 @@
 ﻿
 namespace KHOD.GUI
 {
-	partial class frmNhapKho
+	partial class frmXuatKho
 	{
 		/// <summary>
 		/// Required designer variable.
@@ -30,6 +30,8 @@ namespace KHOD.GUI
 		private void InitializeComponent()
 		{
 			this.panel1 = new System.Windows.Forms.Panel();
+			this.thanhtoan = new System.Windows.Forms.ComboBox();
+			this.labelControl6 = new DevExpress.XtraEditors.LabelControl();
 			this.lbGia = new DevExpress.XtraEditors.LabelControl();
 			this.labelControl5 = new DevExpress.XtraEditors.LabelControl();
 			this.date1 = new System.Windows.Forms.DateTimePicker();
@@ -42,8 +44,6 @@ namespace KHOD.GUI
 			this.labelControl1 = new DevExpress.XtraEditors.LabelControl();
 			this.panel2 = new System.Windows.Forms.Panel();
 			this.dataGridView1 = new System.Windows.Forms.DataGridView();
-			this.labelControl6 = new DevExpress.XtraEditors.LabelControl();
-			this.thanhtoan = new System.Windows.Forms.ComboBox();
 			this.panel1.SuspendLayout();
 			this.panel2.SuspendLayout();
 			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
@@ -67,107 +67,117 @@ namespace KHOD.GUI
 			this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
 			this.panel1.Location = new System.Drawing.Point(0, 0);
 			this.panel1.Name = "panel1";
-			this.panel1.Size = new System.Drawing.Size(946, 236);
+			this.panel1.Size = new System.Drawing.Size(995, 268);
 			this.panel1.TabIndex = 0;
-			this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+			// 
+			// thanhtoan
+			// 
+			this.thanhtoan.FormattingEnabled = true;
+			this.thanhtoan.Location = new System.Drawing.Point(744, 109);
+			this.thanhtoan.Name = "thanhtoan";
+			this.thanhtoan.Size = new System.Drawing.Size(121, 24);
+			this.thanhtoan.TabIndex = 41;
+			// 
+			// labelControl6
+			// 
+			this.labelControl6.Location = new System.Drawing.Point(597, 109);
+			this.labelControl6.Name = "labelControl6";
+			this.labelControl6.Size = new System.Drawing.Size(141, 17);
+			this.labelControl6.TabIndex = 40;
+			this.labelControl6.Text = "Hình thức thanh toán :";
 			// 
 			// lbGia
 			// 
-			this.lbGia.Location = new System.Drawing.Point(475, 85);
+			this.lbGia.Location = new System.Drawing.Point(468, 109);
 			this.lbGia.Name = "lbGia";
 			this.lbGia.Size = new System.Drawing.Size(17, 16);
-			this.lbGia.TabIndex = 27;
+			this.lbGia.TabIndex = 39;
 			this.lbGia.Text = "gia";
 			// 
 			// labelControl5
 			// 
-			this.labelControl5.Location = new System.Drawing.Point(398, 137);
+			this.labelControl5.Location = new System.Drawing.Point(391, 161);
 			this.labelControl5.Name = "labelControl5";
 			this.labelControl5.Size = new System.Drawing.Size(64, 17);
-			this.labelControl5.TabIndex = 26;
+			this.labelControl5.TabIndex = 38;
 			this.labelControl5.Text = "Số lượng :";
 			// 
 			// date1
 			// 
 			this.date1.Font = new System.Drawing.Font("Tahoma", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.date1.Location = new System.Drawing.Point(123, 78);
+			this.date1.Location = new System.Drawing.Point(116, 102);
 			this.date1.Name = "date1";
 			this.date1.Size = new System.Drawing.Size(182, 23);
-			this.date1.TabIndex = 25;
+			this.date1.TabIndex = 37;
 			this.date1.Value = new System.DateTime(2022, 12, 29, 0, 0, 0, 0);
-			this.date1.ValueChanged += new System.EventHandler(this.date1_ValueChanged);
 			// 
 			// simpleButton1
 			// 
 			this.simpleButton1.Appearance.Font = new System.Drawing.Font("Tahoma", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
 			this.simpleButton1.Appearance.Options.UseFont = true;
-			this.simpleButton1.Location = new System.Drawing.Point(800, 190);
+			this.simpleButton1.Location = new System.Drawing.Point(793, 214);
 			this.simpleButton1.Name = "simpleButton1";
 			this.simpleButton1.Size = new System.Drawing.Size(146, 40);
-			this.simpleButton1.TabIndex = 24;
-			this.simpleButton1.Text = "Nhập kho";
-			this.simpleButton1.Click += new System.EventHandler(this.simpleButton1_Click);
+			this.simpleButton1.TabIndex = 36;
+			this.simpleButton1.Text = "Xuất kho";
 			// 
 			// txtSl
 			// 
 			this.txtSl.Font = new System.Drawing.Font("Tahoma", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.txtSl.Location = new System.Drawing.Point(475, 131);
+			this.txtSl.Location = new System.Drawing.Point(468, 155);
 			this.txtSl.Name = "txtSl";
 			this.txtSl.Size = new System.Drawing.Size(82, 23);
-			this.txtSl.TabIndex = 23;
-			this.txtSl.TextChanged += new System.EventHandler(this.txtSl_TextChanged);
+			this.txtSl.TabIndex = 35;
 			// 
 			// labelControl4
 			// 
-			this.labelControl4.Location = new System.Drawing.Point(398, 85);
+			this.labelControl4.Location = new System.Drawing.Point(391, 109);
 			this.labelControl4.Name = "labelControl4";
 			this.labelControl4.Size = new System.Drawing.Size(49, 16);
-			this.labelControl4.TabIndex = 22;
+			this.labelControl4.TabIndex = 34;
 			this.labelControl4.Text = "Đơn giá:";
 			// 
 			// labelControl2
 			// 
 			this.labelControl2.Appearance.Font = new System.Drawing.Font("Tahoma", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
 			this.labelControl2.Appearance.Options.UseFont = true;
-			this.labelControl2.Location = new System.Drawing.Point(583, 12);
+			this.labelControl2.Location = new System.Drawing.Point(576, 36);
 			this.labelControl2.Name = "labelControl2";
-			this.labelControl2.Size = new System.Drawing.Size(138, 34);
-			this.labelControl2.TabIndex = 21;
-			this.labelControl2.Text = "Nhạp Kho";
+			this.labelControl2.Size = new System.Drawing.Size(129, 34);
+			this.labelControl2.TabIndex = 33;
+			this.labelControl2.Text = "Xuất Kho";
 			// 
 			// lbten
 			// 
-			this.lbten.Location = new System.Drawing.Point(208, 137);
+			this.lbten.Location = new System.Drawing.Point(201, 161);
 			this.lbten.Name = "lbten";
 			this.lbten.Size = new System.Drawing.Size(97, 17);
-			this.lbten.TabIndex = 20;
+			this.lbten.TabIndex = 32;
 			this.lbten.Text = "Tên nguyên liệu";
-			this.lbten.Click += new System.EventHandler(this.lbten_Click);
 			// 
 			// labelControl3
 			// 
-			this.labelControl3.Location = new System.Drawing.Point(62, 137);
+			this.labelControl3.Location = new System.Drawing.Point(55, 161);
 			this.labelControl3.Name = "labelControl3";
 			this.labelControl3.Size = new System.Drawing.Size(112, 17);
-			this.labelControl3.TabIndex = 19;
+			this.labelControl3.TabIndex = 31;
 			this.labelControl3.Text = "Tên Nguyên Liệu :";
 			// 
 			// labelControl1
 			// 
-			this.labelControl1.Location = new System.Drawing.Point(77, 83);
+			this.labelControl1.Location = new System.Drawing.Point(70, 107);
 			this.labelControl1.Name = "labelControl1";
 			this.labelControl1.Size = new System.Drawing.Size(37, 16);
-			this.labelControl1.TabIndex = 18;
+			this.labelControl1.TabIndex = 30;
 			this.labelControl1.Text = "Ngày :";
 			// 
 			// panel2
 			// 
-			this.panel2.BackColor = System.Drawing.SystemColors.ButtonFace;
 			this.panel2.Controls.Add(this.dataGridView1);
 			this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
-			this.panel2.Location = new System.Drawing.Point(0, 236);
+			this.panel2.Location = new System.Drawing.Point(0, 268);
 			this.panel2.Name = "panel2";
-			this.panel2.Size = new System.Drawing.Size(946, 487);
+			this.panel2.Size = new System.Drawing.Size(995, 251);
 			this.panel2.TabIndex = 1;
 			// 
 			// dataGridView1
@@ -178,36 +188,19 @@ namespace KHOD.GUI
 			this.dataGridView1.Name = "dataGridView1";
 			this.dataGridView1.RowHeadersWidth = 51;
 			this.dataGridView1.RowTemplate.Height = 24;
-			this.dataGridView1.Size = new System.Drawing.Size(946, 487);
+			this.dataGridView1.Size = new System.Drawing.Size(995, 251);
 			this.dataGridView1.TabIndex = 0;
 			// 
-			// labelControl6
-			// 
-			this.labelControl6.Location = new System.Drawing.Point(604, 85);
-			this.labelControl6.Name = "labelControl6";
-			this.labelControl6.Size = new System.Drawing.Size(141, 17);
-			this.labelControl6.TabIndex = 28;
-			this.labelControl6.Text = "Hình thức thanh toán :";
-			// 
-			// thanhtoan
-			// 
-			this.thanhtoan.FormattingEnabled = true;
-			this.thanhtoan.Location = new System.Drawing.Point(674, 80);
-			this.thanhtoan.Name = "thanhtoan";
-			this.thanhtoan.Size = new System.Drawing.Size(121, 24);
-			this.thanhtoan.TabIndex = 29;
-			// 
-			// frmNhapKho
+			// frmXuatKho
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(946, 723);
+			this.ClientSize = new System.Drawing.Size(995, 519);
 			this.Controls.Add(this.panel2);
 			this.Controls.Add(this.panel1);
 			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-			this.Name = "frmNhapKho";
-			this.Text = "frmNhapKho";
-			this.Load += new System.EventHandler(this.frmNhapKho_Load);
+			this.Name = "frmXuatKho";
+			this.Text = "frmXuatKho";
 			this.panel1.ResumeLayout(false);
 			this.panel1.PerformLayout();
 			this.panel2.ResumeLayout(false);
@@ -219,7 +212,8 @@ namespace KHOD.GUI
 		#endregion
 
 		private System.Windows.Forms.Panel panel1;
-		private System.Windows.Forms.Panel panel2;
+		private System.Windows.Forms.ComboBox thanhtoan;
+		private DevExpress.XtraEditors.LabelControl labelControl6;
 		private DevExpress.XtraEditors.LabelControl lbGia;
 		private DevExpress.XtraEditors.LabelControl labelControl5;
 		private System.Windows.Forms.DateTimePicker date1;
@@ -230,8 +224,7 @@ namespace KHOD.GUI
 		private DevExpress.XtraEditors.LabelControl lbten;
 		private DevExpress.XtraEditors.LabelControl labelControl3;
 		private DevExpress.XtraEditors.LabelControl labelControl1;
+		private System.Windows.Forms.Panel panel2;
 		private System.Windows.Forms.DataGridView dataGridView1;
-		private System.Windows.Forms.ComboBox thanhtoan;
-		private DevExpress.XtraEditors.LabelControl labelControl6;
 	}
 }
